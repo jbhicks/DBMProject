6405d6df94b57ef4c1a3117b700f276642e3c078
jbhicks <jbhicks11b@gmail.com>
2013-10-20 21:08:07 -0600

    Basic login form added

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
+// Setter
 angular.module('myApp', [
   'ngRoute',
   'myApp.filters',
@@ -10,7 +11,12 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/login', {templateUrl: 'login.html', controller: 'LoginController'});
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+myApp.controller('LoginController', function() {
+
+});
